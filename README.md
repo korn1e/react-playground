7cb332f3bd9163b3b77f0b19ab86eb13d106ff80
Aji Perdana Putra <aji_perdana@yahoo.com>
2021-01-01 21:59:45 +0100

    React Hook (44)

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-Basic React example
+Basic React example.
+
+Managing state on functional component using React Hook.
+
+Allow to add functionality to functional component.
--- src/App.js
+++ src/App.js
@@ -1,50 +1,68 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+// Hooks start with use* keyword
+
 //import logo from './logo.svg';
 import './App.css';
 
 import Person from './Person/Person';
 
-class App extends Component {
+const app = props => {
 
-    //built-in property
-    state = {
-      persons: [
-          {name: 'Aji', age:39},
-          {name: 'Bina', age:38},
-          {name: 'Wira', age:34}
-      ]
-    };
+    // useState returns array:
+    // 0: reference to access the state
+    // 1: function that allowed to modify the state
+    // use "[personsState, setPersonsState]" instead of "stateArr"
+    const [personsState, setPersonsState] = useState({
+        persons: [
+            {name: 'Aji', age:39},
+            {name: 'Bina', age:38},
+            {name: 'Wira', age:34}
+        ],
+        otherData: 'other value'
+    });
 
-    switchNameHandler = () => {
+    const [otherState, setOtherState] = useState('other statue value');
+
+    // you can use as many 'useState' as you want, and it does not have to be a JSON object.
+
+    console.log(personsState, otherState);
+
+    // define function inside function
+    const switchNameHandler = () => {
         console.log('switchNameHandler clicked!');
 
         // Do not mutate state directly. Use setState()
         // react/no-direct-mutation-state
-        //this.state.persons[0].name = 'Aji Perdana Putra';
+        //personsState.persons[0].name = 'Aji Perdana Putra';
 
-        //use built-in method (setState)
-        this.setState({persons: [
+        // Rect Hook DOES NOT MERGE the data....instead, it will replace the data
+        // -> copy the old "otherData"
+        setPersonsState({persons: [
                 {name: 'Aji Perdana Putra', age:39},
                 {name: 'Bina', age:38},
                 {name: 'Wira', age:22}
-            ]});
+            ],
+            otherData: personsState.otherData
+        });
     }
 
-    render() {
-        return (
-            // custom component should have capital 1st letter (naming conventional)
-            // lower case 1st letter usually for reserved/internal component
-            <div className="App">
-                <h1>Hi, I'm a react app</h1>
-                <button onClick={this.switchNameHandler}>Change Data</button>
-                <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-                <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobby is eat a lot</Person>
-                <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
-            </div>
-            //equivalent
-            //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a react app'));
-        );
-    }
+
+    return (
+        // custom component should have capital 1st letter (naming conventional)
+        // lower case 1st letter usually for reserved/internal component
+        <div className="App">
+            <h1>Hi, I'm a react app</h1>
+            <button onClick={switchNameHandler}>Change Data</button>
+            <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
+            <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>My hobby is eat a lot</Person>
+            <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
+        </div>
+        //equivalent
+        //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a react app'));
+    );
+
 }
 
-export default App;
+export default app;
+
+
